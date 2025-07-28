e8372f1f65d3095eb7acf82bcaff61fdd4529b07
Uribe <isaias.uribe@aptiv.com>
2025-07-28 09:39:24 -0600

    Actualizacion para archivos de hasta 10mb

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 const db = new sqlite3.Database('./criticos.db');
 
 // --- CREACIÓN DE TABLAS ---
